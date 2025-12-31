7780ae44f8d2a361144a1f5e45694be82a64c142
Emre Yardimci <48123968+emreyardimci@users.noreply.github.com>
2025-12-31 11:50:45 +0300

    Update HelloWorldApplication.java

--- src/main/java/com/quasys/demo/HelloWorldApplication.java
+++ src/main/java/com/quasys/demo/HelloWorldApplication.java
@@ -2,6 +2,6 @@ package com.quasys.demo;
 
 public class HelloWorldApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World from Jenkins!");
+        System.out.println("Hello World from THY!");
     }
 }
