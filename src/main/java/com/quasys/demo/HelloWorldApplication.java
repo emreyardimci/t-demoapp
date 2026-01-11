4d0377aba816da96cdecdc681a1ae412d3592425
Emre Yardimci <emre.yardimci@quasys.com.tr>
2026-01-12 01:09:27 +0300

    emre

--- src/main/java/com/quasys/demo/HelloWorldApplication.java
+++ src/main/java/com/quasys/demo/HelloWorldApplication.java
@@ -4,7 +4,8 @@ public class HelloWorldApplication {
     public static void main(String[] args) {
         while (true) {
             System.out.println("Hello World from THY!");
-			System.out.println("APP_ENV = " + System.getenv("APP_ENV"));
+			System.out.println("APP_DB_CRED = " + System.getenv("APP_DB_CRED"));
+			System.out.println("APP_API_KEY = " + System.getenv("APP_API_KEY"));
             try {
                 Thread.sleep(5_000); // 5 saniye
             } catch (InterruptedException e) {
