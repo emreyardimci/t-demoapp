0fc63b03974a7515fc641c43d8a398c7675d152b
Emre Yardimci <emre.yardimci@quasys.com.tr>
2026-01-12 01:02:13 +0300

    emre

--- src/main/java/com/quasys/demo/HelloWorldApplication.java
+++ src/main/java/com/quasys/demo/HelloWorldApplication.java
@@ -2,10 +2,11 @@ package com.quasys.demo;
 
 public class HelloWorldApplication {
     public static void main(String[] args) {
-                while (true) {
+        while (true) {
             System.out.println("Hello World from THY!");
+			System.out.println("APP_ENV = " + System.getenv("APP_ENV"));
             try {
-                Thread.sleep(5_000); 
+                Thread.sleep(5_000); // 5 saniye
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 break;
