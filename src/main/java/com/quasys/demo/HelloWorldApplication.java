ff793e0c2937320c9a3aa304268ea0b928a02bf5
Emre Yardimci <emre.yardimci@quasys.com.tr>
2026-01-12 14:31:21 +0300

    emre

--- src/main/java/com/quasys/demo/HelloWorldApplication.java
+++ src/main/java/com/quasys/demo/HelloWorldApplication.java
@@ -11,6 +11,10 @@ public class HelloWorldApplication {
         System.setProperty("APP_POSTGRESQL_PRIVATEKEY", "ThisIsCertPrivateKey");
         System.out.println("APP_POSTGRESQL_PRIVATEKEY: " + System.getProperty("APP_POSTGRESQL_PRIVATEKEY"));
 
+        BeyondTrustClient client = new BeyondTrustClient();
+        String secret = client.getSecretText("3eff81ac-5112-4cfc-6361-08de51b90d05");
+        System.out.println("APP_POSTGRESQL_BEYONDTRUST_SECRET: " + secret);
+
         Thread.sleep(5_000);
         }
     }
