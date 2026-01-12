8bd30ce68812b8be1e09d17e9b44f04ec05e6f92
Emre Yardimci <emre.yardimci@quasys.com.tr>
2026-01-12 14:10:21 +0300

    emre

--- src/main/java/com/quasys/demo/HelloWorldApplication.java
+++ src/main/java/com/quasys/demo/HelloWorldApplication.java
@@ -2,8 +2,11 @@ package com.quasys.demo;
 
 public class HelloWorldApplication {
     public static void main(String[] args)  throws Exception {
+        while (true) {
+
         System.out.println("Hello World from THY!");
 
-
+        Thread.sleep(5_000);
+        }
     }
 }
