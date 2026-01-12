e6f1f5ee2ef96917d3a60e278eced7bf138c8d3d
Emre Yardimci <emre.yardimci@quasys.com.tr>
2026-01-12 12:34:04 +0300

    emre

--- src/main/java/com/quasys/demo/HelloWorldApplication.java
+++ src/main/java/com/quasys/demo/HelloWorldApplication.java
@@ -4,13 +4,6 @@ public class HelloWorldApplication {
     public static void main(String[] args)  throws Exception {
         System.out.println("Hello World from THY!");
 
-        BeyondTrustClient client = new BeyondTrustClient();
 
-        String secret = client.getSecretText(
-                "843d6517-0fb6-43f0-809e-08dc4eff0fb3"
-        );
-
-        System.out.println("SECRET TEXT:");
-        System.out.println(secret);
     }
 }
