37c75642c1507f91084d21085bfcbb45b45fb517
Emre Yardimci <emre.yardimci@quasys.com.tr>
2026-01-12 11:38:28 +0300

    emre

--- src/main/java/com/quasys/demo/HelloWorldApplication.java
+++ src/main/java/com/quasys/demo/HelloWorldApplication.java
@@ -2,18 +2,6 @@ package com.quasys.demo;
 
 public class HelloWorldApplication {
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("Hello World from THY!");
-			System.out.println("APP_DB_CRED = " + System.getenv("APP_DB_CRED"));
-			System.out.println("APP_API_KEY = " + System.getenv("APP_API_KEY"));
-			System.setProperty("APP_MY_CERT", "this is cert private key");
-			System.out.println("APP_MY_CERT = " + System.getProperty("APP_MY_CERT"));
-            try {
-                Thread.sleep(5_000); // 5 saniye
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                break;
-            }
-        }
+        System.out.println("Hello World from THY!");
     }
 }
